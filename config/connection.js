08b7f1e9821f696ed73807e738aa0ccbbde4d9fd
cdt12988 <cdt12988@gmail.com>
2018-11-29 00:01:17 -0600

    added JAWSDB connection

--- config/connection.js
+++ config/connection.js
@@ -1,16 +1,20 @@
 var mysql = require('mysql');
 
-var connection = mysql.createConnection({
-	host: process.env.DB_HOST,
-	user: process.env.DB_USER,
-	password: process.env.DB_PASSWORD,
-	database: process.env.DB_NAME
-});
-
 //	If socketPath is needed as a connection property:
 //		* Find your mysql socket path in terminal by typing: netstat -ln | grep mysql
 //		* Copy the path and add the following property-value pair after the database: DB_NAME, pair:
 //		* socketPath: 'path/to/copied/socket'
+
+if(process.env.JAWSDB_URL){
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+	connection = mysql.createConnection({
+		host: process.env.DB_HOST,
+		user: process.env.DB_USER,
+		password: process.env.DB_PASSWORD,
+		database: process.env.DB_NAME
+	});
+}
 
 connection.connect((err) => {
 	if(err) {
